c375d4df9d7fa93ea1465ca1fcb8aad565152d5c
ahmad <aalmadani111@gmail.com>
2023-01-23 04:48:33 +0100

    start the project

---
+++ java.js
@@ -0,0 +1,7 @@
+
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar-fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
