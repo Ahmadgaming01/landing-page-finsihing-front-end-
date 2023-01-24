3f00b460526e279d4b8000bef952902410e0d1da
ahmad <aalmadani111@gmail.com>
2023-01-24 13:59:32 +0100

    remove the script

--- java.js
+++ java.js
@@ -1,7 +1,7 @@
 
-$(function () {
-    $(document).scroll(function () {
-      var $nav = $(".navbar-fixed-top");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-  });
+//  (function () {
+//     $(document).scroll(function () {
+//       var $nav = $(".navbar-fixed-top");
+//       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+//     });
+//   });
